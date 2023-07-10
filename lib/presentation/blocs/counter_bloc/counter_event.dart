d7ba9a447e43add2d48c0a279f42179e22c0da3e
Jose Ona <onapinedajose@gmail.com>
2023-07-10 17:32:28 +0200

    Implementado BLoC

---
+++ lib/presentation/blocs/counter_bloc/counter_event.dart
@@ -0,0 +1,14 @@
+part of 'counter_bloc.dart';
+
+//Qué tipos de eventos vamos a recibir
+abstract class CounterEvent  {
+  const CounterEvent();
+
+  
+}
+
+class CounterIncreased extends CounterEvent{
+  final int value;
+  
+  const CounterIncreased(this.value);
+}
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
           ),
           ListTile(
             title: const Text('Bloc'),
-            subtitle: const Text('Gestor de estado simple'),
+            subtitle: const Text('Gestor de estado compuesto'),
             trailing: const Icon(Icons.arrow_forward_ios_rounded),
             onTap: () => context.push('/bloc'),
           )
