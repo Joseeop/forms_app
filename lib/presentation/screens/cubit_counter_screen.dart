0cb7adf3fbfc7b1c4463544472d3914b540898f5
Jose Ona <onapinedajose@gmail.com>
2023-07-10 16:42:15 +0200

    Usando Cubit

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,50 +1,66 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:form_app/presentation/blocs/counter_cubit/counter_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => CounterCubit(), child: const _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Cubit Counter'),
+        title:  Text('Cubit Counter: ${counterState.transactionCount} '),
         actions: [
-          IconButton(
-            onPressed: (){}, 
-          icon: const Icon(Icons.refresh_rounded))
+          IconButton(onPressed: () {}, icon: const Icon(Icons.refresh_rounded))
         ],
       ),
-      body: const Center(
-        child: Text('Counter Value: xxxx'),
+      body:  Center(
+        child: BlocBuilder<CounterCubit, CounterState>(
+          //buildWhen: (previous, current) => current.counter != previous.counter,
+          builder: (context, state) {
+            return Text('Counter Value: ${state.counter}');
+          },
+        ),
       ),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed:() => {},
-          heroTag: '1',
-          child: const Text('+3'),
+            onPressed: () => {},
+            heroTag: '1',
+            child: const Text('+3'),
           ),
-          const SizedBox(height: 15,
-          ),
-          FloatingActionButton(
-            onPressed:() => {},
-          heroTag: '2',
-          child: const Text('+2'),
-          ),
-          const SizedBox(height: 15,
+          const SizedBox(
+            height: 15,
           ),
           FloatingActionButton(
-            onPressed:() => {},
-          heroTag: '3',
-          child: const Text('+1'),
+            onPressed: () => {},
+            heroTag: '2',
+            child: const Text('+2'),
           ),
-          
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            onPressed: () => {},
+            heroTag: '3',
+            child: const Text('+1'),
+          ),
         ],
       ),
-      
     );
   }
 }
