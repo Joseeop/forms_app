229ecc61a00db17b2c3bed3f1504aae5f5358b36
Jose Ona <onapinedajose@gmail.com>
2023-07-14 11:16:33 +0200

    Inicio de register Cubit

---
+++ lib/presentation/blocs/register/register_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterFormState> {
+  RegisterCubit() : super(const RegisterFormState());
+
+
+  
+}
